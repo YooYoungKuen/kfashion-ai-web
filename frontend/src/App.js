de0e0f2801166ed4a3c86e54ccf50bd538fddea9
kimcy2 <kbsc9090@gmail.com>
2025-10-21 20:36:44 +0900

    remove submodule and re-add frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./pages/Home"; // ✅ Home.jsx 경로 맞게
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
